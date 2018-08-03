6496b0751440b89723258cc4104e947b87322470
songjinze <734609160@qq.com>
2018-08-03 14:56:37 +0800

    update

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+    public static GameManager instance = null;
+
+    private int level = 3;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+}
